a93d265c495913dedfb6b6eaf9f1c4d5a71d6fd5
msirota <marina.sirota@ucsf.edu>
2019-02-13 22:03:41 -0800

    Revert "Add files via upload"

--- js/nav-toggle.js
+++
@@ -1,8 +0,0 @@
-function toggleNav() {
-  var x = document.getElementById("responsiveNav");
-  if (x.className === "responsive-nav") {
-    x.className += " responsive";
-  } else {
-    x.className = "responsive-nav";
-  }
-}
